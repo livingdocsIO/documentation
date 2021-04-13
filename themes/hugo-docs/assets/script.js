f8ae983ea108ab10c8b4e08261ed8528aa73bc35
svenlivingdocs <sven@livingdocs.io>
2021-04-13 14:40:35 +0200

    chore(scripts and markup): Updated
    - Updated scripts and markup to reflect structural and visual changes

--- themes/hugo-docs/assets/script.js
+++ themes/hugo-docs/assets/script.js
@@ -6,12 +6,12 @@ for (const elem of document.querySelectorAll('.highlight')) {
   if (elem.innerText.length < 5) continue
 
   const copyElem = document.createElement('span')
-  copyElem.className = 'copy-btn'
+  copyElem.className = 'copy-button'
   copyElem.innerText = 'Copy'
   elem.appendChild(copyElem, elem.nextSibling)
 }
 
-const clip = new Clipboard('.copy-btn', {
+const clip = new Clipboard('.copy-button', {
   text (btn) { return btn.previousSibling.innerText.replace(/^\$\s/gm, '') }
 })
 
@@ -45,13 +45,13 @@ function appendAnchor(heading) {
   const anchor = document.createElement('a')
   anchor.href = `#${id}`
   anchor.className = 'anchor'
-  anchor.innerHTML = `<svg viewBox="0 0 24 24"><title>link-variant</title><path d="M10.59,13.41C11,13.8 11,14.44 10.59,14.83C10.2,15.22 9.56,15.22 9.17,14.83C7.22,12.88 7.22,9.71 9.17,7.76V7.76L12.71,4.22C14.66,2.27 17.83,2.27 19.78,4.22C21.73,6.17 21.73,9.34 19.78,11.29L18.29,12.78C18.3,11.96 18.17,11.14 17.89,10.36L18.36,9.88C19.54,8.71 19.54,6.81 18.36,5.64C17.19,4.46 15.29,4.46 14.12,5.64L10.59,9.17C9.41,10.34 9.41,12.24 10.59,13.41M13.41,9.17C13.8,8.78 14.44,8.78 14.83,9.17C16.78,11.12 16.78,14.29 14.83,16.24V16.24L11.29,19.78C9.34,21.73 6.17,21.73 4.22,19.78C2.27,17.83 2.27,14.66 4.22,12.71L5.71,11.22C5.7,12.04 5.83,12.86 6.11,13.65L5.64,14.12C4.46,15.29 4.46,17.19 5.64,18.36C6.81,19.54 8.71,19.54 9.88,18.36L13.41,14.83C14.59,13.66 14.59,11.76 13.41,10.59C13,10.2 13,9.56 13.41,9.17Z"></path></svg>`
+  anchor.innerHTML = `<svg viewBox="0 0 24 24"><title>link-variant</title><path fill="currentColor" d="M10.59,13.41C11,13.8 11,14.44 10.59,14.83C10.2,15.22 9.56,15.22 9.17,14.83C7.22,12.88 7.22,9.71 9.17,7.76V7.76L12.71,4.22C14.66,2.27 17.83,2.27 19.78,4.22C21.73,6.17 21.73,9.34 19.78,11.29L18.29,12.78C18.3,11.96 18.17,11.14 17.89,10.36L18.36,9.88C19.54,8.71 19.54,6.81 18.36,5.64C17.19,4.46 15.29,4.46 14.12,5.64L10.59,9.17C9.41,10.34 9.41,12.24 10.59,13.41M13.41,9.17C13.8,8.78 14.44,8.78 14.83,9.17C16.78,11.12 16.78,14.29 14.83,16.24V16.24L11.29,19.78C9.34,21.73 6.17,21.73 4.22,19.78C2.27,17.83 2.27,14.66 4.22,12.71L5.71,11.22C5.7,12.04 5.83,12.86 6.11,13.65L5.64,14.12C4.46,15.29 4.46,17.19 5.64,18.36C6.81,19.54 8.71,19.54 9.88,18.36L13.41,14.83C14.59,13.66 14.59,11.76 13.41,10.59C13,10.2 13,9.56 13.41,9.17Z"></path></svg>`
   heading.insertBefore(anchor, heading.firstChild)
   heading.insertBefore(anchorOffset, heading.firstChild)
   heading.removeAttribute('id')
 }
 
-const anchorElements = document.querySelectorAll('.content-wrapper h2, .content-wrapper h3, .content-wrapper h4')
+const anchorElements = document.querySelectorAll('.page__content h2, .page__content h3, .page__content h4')
 for (const elem of anchorElements) appendAnchor(elem)
 
 // Add target = _blank to all external links.
@@ -69,18 +69,18 @@ const searchModal = search.querySelector('.search-results')
 
 let documentResultsTemplate = document.createElement('div')
 documentResultsTemplate.innerHTML = [
-  `<div class="search-results--document">`,
-      `<div class="title"></div>`,
-      `<div class="results"></div>`,
+  `<div class="search-results-document">`,
+      `<div class="search-results-document__tags"></div>`,
+      `<div class="search-results-document__content"></div>`,
   `</div>`
 ].join('')
 documentResultsTemplate = documentResultsTemplate.firstElementChild
 
 let searchResultTemplate = document.createElement('div')
 searchResultTemplate.innerHTML = [
-  `<a class="search-results--entry">`,
-      `<div class="title"></div>`,
-      `<div class="description"></div>`,
+  `<a class="search-results-line">`,
+      `<div class="search-results-line__title"></div>`,
+      `<div class="search-results-line__text"></div>`,
   `</a>`
 ].join('')
 searchResultTemplate = searchResultTemplate.firstElementChild
@@ -172,3 +172,11 @@ function createSearchWorker () {
     }
   }
 }
+
+// Mobile burger navigation
+const burger = document.querySelector('.header__burger')
+const page = document.querySelector('.page')
+
+burger.addEventListener('click', (evt) => {
+  page.classList.toggle('show-sidebar')
+})
