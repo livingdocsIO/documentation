5883a1043ff55e2e64d696ab83b0e84e1b137af8
Alex Wild <alex.j.wild@gmail.com>
2023-07-14 12:38:25 +0200

    fix(search): Improve metadata search results

--- themes/hugo-docs/assets/search.js
+++ themes/hugo-docs/assets/search.js
@@ -11,13 +11,12 @@ function initializeIndex (searchJson) {
 		.then(response => response.json())
 		.then((documents) => {
 			index = lunr(function () {
-				// this.pipeline.remove(lunr.trimmer)
-
 				this.ref('url')
-				this.field('title')
-				this.field('description')
+				this.field('title', {boost: 50})
+				this.field('subtitle', {boost: 10})
+				this.field('description', {boost: 2})
 				this.field('body')
-				this.field('keywords')
+				this.field('keywords', {boost: 5})
 				this.metadataWhitelist = ['position']
 
 				for (const doc of documents) {
@@ -47,16 +46,15 @@ function initializeIndex (searchJson) {
 
 function searchInIndex (index, {query, filterTags, limit}) {
 	const byDoc = {}
-	let count = 0
-	for (const match of index.query(queryFunction.bind(this, index, query))) {
-		count++
+	for (const match of index.search(query)) {
 		const ref = byRef.get(match.ref)
 		const documentUrl = match.ref.replace(/#.+/, '')
 		const doc = byRef.get(documentUrl)
 		const byField = matchesOfFields(match)
 
 		const url = ref.url
-		const title = highlight('title', ref.title, byField)
+		const titleOrSubtitle = ref.title || ref.subtitle
+		const title = highlight('title', titleOrSubtitle, byField)
 		const description = highlight('description', ref.description, byField)
 		const tags = ref.section ? ref.section.split(',').map((l) => l.trim() ? `<div class="tag tag--spaced">${l.trim()}</div>` : '').join('') : ''
 
@@ -66,10 +64,12 @@ function searchInIndex (index, {query, filterTags, limit}) {
       byDoc[documentUrl] = byDoc[documentUrl] || {
         score: 0,
         results: [],
-        title: `${tags}${doc.title === ref.title ? '' : `<a href="${documentUrl}" class="search-results-document__title">${doc.title}</a>`}`
+        title: `${tags}${doc.title === (titleOrSubtitle) ? '' : `<a href="${documentUrl}" class="search-results-document__title">${doc.title}</a>`}`
       }
       byDoc[documentUrl].results.push({url, title, description})
-      byDoc[documentUrl].score += match.score
+			if (match.score > byDoc[documentUrl].score) {
+				byDoc[documentUrl].score = match.score
+			}
     }
 	}
 
@@ -108,30 +108,6 @@ function highlight (prop, string, matches) {
 	}
 	str += string.substr(offset)
 	return str
-}
-
-function queryFunction (index, query, q) {
-	const words = query.toLowerCase().split(/[ .]/).filter(Boolean)
-
-	// how to properly normalize the string?
-	const segments = words.map((k) => index.pipeline.runString(k)[0])
-
-	q.term(words, {
-		usePipeline: true,
-		editDistance: 1,
-		boost: 50,
-		fields: ['title', 'description'],
-		wildcard: lunr.Query.wildcard.TRAILING
-	})
-
-	q.term(segments, {
-		fields: ['title', 'description', 'body'],
-		boost: 5,
-		editDistance: 0,
-		usePipeline: false
-	})
-
-  new lunr.QueryParser(query, q).parse()
 }
 
 self.addEventListener('message', function (e) {
